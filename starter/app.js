6685ec295c6cc3ed82d5726f9b6fa9de80c83c6e
Shemzoo <46892255+Shemzoo@users.noreply.github.com>
2020-03-26 12:59:48 +0300

    sets application modules

---
+++ starter/app.js
@@ -0,0 +1,34 @@
+'use strict';
+
+let budgetController = (function() {
+    let x = 23;
+
+    let add = function(a) {
+        return x + a;
+    }
+
+    return {
+        publicTest: function(b) {
+            return add(b);
+        }
+    }
+
+})();
+
+let UIController = (function() {
+
+    // Some code
+    
+})();
+
+let controller = (function(budgetCtrl, UICtrl) {
+
+    var z = budgetCtrl.publicTest(5);
+
+    return {
+        anotherPublicTest: function() {
+            console.log(z);
+        }
+    }
+
+})(budgetController, UIController);
