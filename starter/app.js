93098a5350da4b138aa4fff96294747027a69818
Shemzoo <46892255+Shemzoo@users.noreply.github.com>
2020-03-30 13:13:05 +0300

    adds data structure

--- starter/app.js
+++ starter/app.js
@@ -11,9 +11,6 @@ let budgetController = (function() {
             this.value = value;
         };
 
-        let allExpenses = [];
-        let allIncomes = [];
-
         let data = {
             allItems: {
                 exp: [],
@@ -25,6 +22,39 @@ let budgetController = (function() {
                 inc: 0
             }
         }
+
+        return {
+            addItem: function(type, des, val) {
+                let newItem, ID;
+
+
+                // create new ID
+                if (data.allItems[type].length > 0) {
+                    ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+                } else {
+                    ID = 0;
+                }
+                
+
+                // create new item based on 'inc' or 'exp' type
+                if (type === 'exp') {
+                    newItem = new Expense(ID, des, val)
+                } else if (type === 'inc') {
+                    newItem = new Income(ID, des, val)
+                }
+
+                // push it into our data sctructure
+                data.allItems[type].push(newItem);
+
+                // return the new element 
+                return newItem;
+            },
+
+            testing: function() {
+                console.log(data);
+            }
+        }
+
 })();
 
 
@@ -73,6 +103,7 @@ let controller = (function(budgetCtrl, UICtrl) {
         let input = UICtrl.getInput(); 
 
         // 2. Add the item to the budget cotroller
+        let newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
         // 3. Add the item to the UI
 
